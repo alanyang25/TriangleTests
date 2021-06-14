6c9fbe16e659284ff80e6a50ed54941bd48f2c39
alanyang25 <61829671+calthorp@users.noreply.github.com>
2021-06-15 01:12:45 +0800

    updates

--- conftest.py
+++ conftest.py
@@ -1,20 +1,21 @@
 import pytest
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="class")
 def chrome_driver_init(request):
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
-    web_driver = webdriver.Chrome(executable_path=r'C:\webdrivers\chromedriver.exe', options=options)
+    web_driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     request.cls.driver = web_driver
     yield
     web_driver.close()
 
-@pytest.fixture(scope="class")
-def firefox_driver_init(request):
-    options = webdriver.FirefoxOptions()
-    options.add_argument("--headless")
-    web_driver = webdriver.Firefox(executable_path=r'C:\webdrivers\geckodriver.exe', options=options)
-    request.cls.driver = web_driver
-    yield
-    web_driver.close()
+# @pytest.fixture(scope="class")
+# def firefox_driver_init(request):
+#     options = webdriver.FirefoxOptions()
+#     options.add_argument("--headless")
+#     web_driver = webdriver.Firefox(executable_path=r'C:\webdrivers\geckodriver.exe', options=options)
+#     request.cls.driver = web_driver
+#     yield
+#     web_driver.close()
